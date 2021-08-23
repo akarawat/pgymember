759e298f9185a325d71bf7f19ae83cec9101a2f7
aradapt <64973857+aradapt@users.noreply.github.com>
2021-08-23 19:19:30 +0700

    commit on main

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ void getVersion() async {
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-// .....
+// ..... 12345
     globals.appVer = ('Failed to load album');
     // throw Exception('Failed to load album');
   }
