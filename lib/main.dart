1966ca35efdb6a92a7fdd11e93692f789420929d
aradapt <64973857+aradapt@users.noreply.github.com>
2021-08-23 19:16:20 +0700

    Add remark

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ void getVersion() async {
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-
+// .....
     globals.appVer = ('Failed to load album');
     // throw Exception('Failed to load album');
   }
